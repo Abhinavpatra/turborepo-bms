11a1022b8ef786f6e872805ccc040d42f9b491ab
Abhinavpatra <149466436+Abhinavpatra@users.noreply.github.com>
2026-02-13 09:14:30 +0530

    modified the index.ts client export, using pg pool for apt type support and function invocations

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,4 +1,9 @@
-import { PrismaClient } from "@prisma/client/extension";
+import "dotenv/config";
+import { PrismaClient } from "./generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-export const client = new PrismaClient();
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+export const client = new PrismaClient({ adapter });
 
